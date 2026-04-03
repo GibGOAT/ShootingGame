10cc068db0bff7fa04180f59717615a69bc61125
GibGOAT <ceesaygibril40@gmail.com>
2026-04-03 19:01:44 -0400

    shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject prefab;
+    public Transform bulletTrash;
+    public Transform bulletspawn;
 
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+            if(_currentTime < 0)
+            {
+                _canShoot = true;
+                _currentTime = Timer;
+            }
+        }
+     if(Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(prefab, bulletspawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+
+    }
 }
