4f0b8e6775c8ab92480962b465126618d76ad822
GibGOAT <ceesaygibril40@gmail.com>
2026-04-04 16:14:05 -0400

    ITS WORKING

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject prefab2;
     public Transform bulletTrash;
     public Transform bulletspawn;
 
@@ -10,25 +11,34 @@ public class PlayerShoot : MonoBehaviour
     private float _currentTime = 0.5f;
     private bool _canShoot = true;
 
-    private void Update()
+    private void Update()//timer
     {
         if (!_canShoot)
         {
             _currentTime -= Time.deltaTime;
-            if(_currentTime < 0)
+            if (_currentTime < 0)
             {
                 _canShoot = true;
                 _currentTime = Timer;
             }
         }
-     if(Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        //bullet1
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
         {
             GameObject bullet = Instantiate(prefab, bulletspawn.position, Quaternion.identity);
-
+           
             bullet.transform.SetParent(bulletTrash);
+         
 
             _canShoot = false;
         }
 
+        //bullet2
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
+        {
+            GameObject bullet2 = Instantiate(prefab2, bulletspawn.position, Quaternion.identity);
+            bullet2.transform.SetParent(bulletTrash);
+
+        }
     }
 }
